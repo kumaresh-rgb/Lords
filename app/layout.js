be01e035404f9fbe085992e70af5ece56d55f785
M kumaresh <kk8180183@gmail.com>
2023-09-22 17:03:01 +0530

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         
         {children}
+        <Analytics />
         </body>
     </html>
   )
